aadfe9213cb7af512213b1a5f2b3d4848702ca94
trungdv <dvantrung@gmail.com>
2011-12-22 15:40:46 +0700

    commit

---
+++ Robe Di Kappa/Robe Di Kappa/Source/Categories/RDKLocationsItem.h
@@ -0,0 +1,13 @@
+//
+//  RDKLocationsItem.h
+//  Robe Di Kappa
+//
+//  Created by Trung Dang on 12/22/11.
+//  Copyright (c) 2011 Interfacelab LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RDKLocationsItem : NSObject
+
+@end
---
+++ Robe Di Kappa/Robe Di Kappa/Source/Categories/RDKLocationsItem.m
@@ -0,0 +1,13 @@
+//
+//  RDKLocationsItem.m
+//  Robe Di Kappa
+//
+//  Created by Trung Dang on 12/22/11.
+//  Copyright (c) 2011 Interfacelab LLC. All rights reserved.
+//
+
+#import "RDKLocationsItem.h"
+
+@implementation RDKLocationsItem
+
+@end
